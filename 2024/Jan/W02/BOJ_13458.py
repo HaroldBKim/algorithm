0e5faf21b1626829386b13bf9b9bbaba8d47efcc
HaroldBKim <sbookim2@gmail.com>
2024-01-10 00:20:36 +0900

    Feat: 시험 감독

---
+++ 2024/Jan/W02/BOJ_13458.py
@@ -0,0 +1,20 @@
+N = int(input())    				
+A = list(map(int, input().split()))  
+B, C = map(int, input().split())     
+cnt = 0 
+
+for i in A: 
+    if i <= B:      
+        cnt += 1    
+        continue    
+        
+    i -= B         
+    cnt += 1       
+    
+    if i % C == 0: 
+        cnt += i//C
+        
+    else :          
+        cnt += i//C + 1
+
+print(cnt) 
